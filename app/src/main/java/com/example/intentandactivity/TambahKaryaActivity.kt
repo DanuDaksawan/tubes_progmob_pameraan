d94c51200f4e8db8ff46965dc1f6fdbf0c9765c0
DanuDaksawan <daksawan@gmail.com>
2024-06-20 02:31:18 +0800

    Menambahkan Main Activity dan Tambah Karya Activity

---
+++ app/src/main/java/com/example/intentandactivity/TambahKaryaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.intentandactivity
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TambahKaryaActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tambah_karya)
+    }
+}
