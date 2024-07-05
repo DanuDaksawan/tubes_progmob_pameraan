36ce72fccc924e37508979d27f7aab6f05d83498
DanuDaksawan <daksawan@gmail.com>
2024-07-05 15:42:57 +0800

    menambahkan DetailKaryaActivity

---
+++ app/src/main/java/com/example/intentandactivity/DetailKaryaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.intentandactivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailKaryaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_karya)
+    }
+}
